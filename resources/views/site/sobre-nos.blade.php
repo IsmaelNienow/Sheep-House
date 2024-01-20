fd0891f4bea4347a3fa2dde31300d31e47ae63bc
Ismael Nienow <isma.hnienow@hotmail.com>
2024-01-20 16:22:44 -0300

    Segundo commit

--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,55 +1,35 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <title>Sheep House - Sobre Nós</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="{{ asset('css/estilo_basico.css')}}">
-    </head>
+@extends('site.layouts.basico')
 
-    <body>
-        <div class="topo">
+@section('titulo','Sobre-Nós')
 
-            <div class="logo">
-                <img src="{{ asset('img/ic_menu.png')}}">
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li><a href="{{ route('site.index') }}">Principal</a></li>
-                    <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                    <li><a href="{{ route('site.contato') }}">Contato</a></li>
-                </ul>
-            </div>
+@section('conteudo')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina">
+            <h1>Olá, eu sou o Sheep House</h1>
         </div>
 
-        <div class="conteudo-pagina">
-            <div class="titulo-pagina">
-                <h1>Olá, eu sou o Sheep House</h1>
-            </div>
+        <div class="informacao-pagina">
+            <p>O Sheep House - Controle de Ovinos é o sistema online de controle e gestão de rebanhos, que pode transformar e potencializar os negócios da sua fazenda.</p>
+            <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
+        </div>  
+    </div>
 
-            <div class="informacao-pagina">
-                <p>O Sheep House - Controle de Ovinos é o sistema online de controle e gestão de rebanhos, que pode transformar e potencializar os negócios da sua fazenda.</p>
-                <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
-            </div>  
+    <div class="rodape">
+        <div class="redes-sociais">
+            <h2>Redes sociais</h2>
+            <img src="{{ asset('img/facebook.png')}}">
+            <img src="{{ asset('img/linkedin.png')}}">
+            <img src="{{ asset('img/youtube.png')}}">
         </div>
-
-        <div class="rodape">
-            <div class="redes-sociais">
-                <h2>Redes sociais</h2>
-                <img src="{{ asset('img/facebook.png')}}">
-                <img src="{{ asset('img/linkedin.png')}}">
-                <img src="{{ asset('img/youtube.png')}}">
-            </div>
-            <div class="area-contato">
-                <h2>Contato</h2>
-                <span>(54)99948-2043</span>
-                <br>
-                <span>isma.hnienow@hotmail.com</span>
-            </div>
-            <div class="localizacao">
-                <h2>Localização</h2>
-                <img src="{{ asset('img/mapa.png')}}">
-            </div>
+        <div class="area-contato">
+            <h2>Contato</h2>
+            <span>(54)99948-2043</span>
+            <br>
+            <span>isma.hnienow@hotmail.com</span>
         </div>
-    </body>
-</html>
+        <div class="localizacao">
+            <h2>Localização</h2>
+            <img src="{{ asset('img/mapa.png')}}">
+        </div>
+    </div>
+@endsection
